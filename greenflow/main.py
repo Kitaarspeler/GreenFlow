eae059e087ae17cc9b60ce24b34da343f068af26
Kitaarspeler <random.3000@hotmail.com>
2023-07-18 18:09:40 +1200

    Testing changes

--- greenflow/main.py
+++ greenflow/main.py
@@ -4,6 +4,7 @@
 import sys
 import RPi.GPIO as GPIO
 from solenoid import Solenoid
+from time import sleep
 
 
 def main():
@@ -12,6 +13,10 @@ def main():
         solenoid1 = Solenoid(11, False)
     except ValueError:
         sys.exit("GPIO pin number not valid")
+
+    solenoid1.toggle()
+    sleep(5)
+    solenoid1.toggle()
 
 if __name__ == "__main__":
     main()
