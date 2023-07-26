bec037212a92f99da6c37effa611211e5c84abc8
Kitaarspeler <random.3000@hotmail.com>
2023-07-27 07:52:09 +1200

    Flask class working

--- greenflow/app.py
+++ greenflow/app.py
@@ -1,78 +1,61 @@
-"""GreenFlow RPi based garden watering system
-
-This program allows the user to set up multiple solenoids for turning
-on and off multiple hoses to water different parts of their garden.
-"""
-
-
-import sys
 import bcrypt
+import logging
 import RPi.GPIO as GPIO
 from database import Database
 from solenoid import Solenoid
 from models import User
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_classful import FlaskView, route
 
 
 app = Flask(__name__)
 app.secret_key = "akl;wejr,q2bjk35jh2wv35tugyaiu"
 
 
-def main():
-    """Initialises GPIO pins, solenoids, schedules and flask app
+class Interface(FlaskView):
+    @classmethod
+    def _initilization(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
-    """
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
+        solenoids = {}
 
-    solenoids = {}
+        for i in range(1, 5):
+            solenoids[i] = Solenoid(i + 1)
+            print(f"Solenoid {i} created: {solenoids[i]}")
 
-    for i in range(1, 5):
-        solenoids[i] = Solenoid(i + 1)
-        print(f"Solenoid {i}: {solenoids[i]}")
-
-    DBINFO = {"host": "localhost", "user": "greenflowuser", "password": "fasc1st-$hoot-c4rbine-WARINESS", "database": "greenflow"}
-
-    salt = bcrypt.gensalt()
-
-    mydb = Database(DBINFO)
-    mydb.write_password("jull", b"hiimashasheepshagger", salt)
-    print(mydb.get_password("jull"))
-
-    app.run(
-        debug = True,
-        host = "0.0.0.0",
-        port = 5000,
-        )
+        DBINFO = {"host": "localhost", "user": "greenflowuser", "password": "fasc1st-$hoot-c4rbine-WARINESS", "database": "greenflow"}
+        salt = bcrypt.gensalt()
+        mydb = Database(DBINFO)
+        #mydb.write_password("jull", b"hiimashasheepshagger", salt)
+        #print(mydb.get_password("jull"))
 
 
-def get_num_solenoids():
-    """Gets and returns the number of solenoids needed for this install
-    
-    Returns
-    -------
-    int
-        The number of solenoids needed for this implementation
+    #@app.route("/")
+    def index(self):
+        return render_template("index.html")
 
-    Raises
-    ------
-    ValueError
-        If there are too many solenoids for the number of GPIO pins
-    """
-
-    num_solenoids = 0
-    while True:
-        if num_solenoids < 1 or num_solenoids > 27:
-            try:
-                num_solenoids = int(input("Please enter the number of solenoids needed for this install: "))
-            except ValueError:
-                pass
+    default_methods = ['GET', 'POST']
+    #@app.route("/login", methods=["GET", "POST"])
+    def login(self):
+        if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+            # Create variables for easy access
+            username = request.form['username']
+            password = request.form['password']
+            if self.is_authenticated(username, password):
+                return redirect(url_for("Interface:index"))
+            else:
+                return render_template("login.html")
         else:
-            break
-    return num_solenoids
+            return render_template("login.html")
+        
 
+    #@app.route("/schedule")
+    def schedule(self):
+        return render_template("schedule.html")
+    
 
-def is_authenticated(username, password):
+    def is_authenticated(self, username, password):
         encoded_pw = password.encode('utf-8')
         hashed = bcrypt.hashpw(encoded_pw, bcrypt.gensalt())
         if bcrypt.checkpw(b'asdf', hashed):
@@ -81,30 +64,15 @@ def is_authenticated(username, password):
             return False
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        password = request.form['password']
-        if is_authenticated(username, password):
-            return redirect(url_for("index"))
-        else:
-            return render_template("login.html")
-    else:
-        return render_template("login.html")
-    
-
-@app.route("/schedule")
-def schedule():
-    return render_template("schedule.html")
+Interface._initilization()
+Interface.register(app, route_base='/')
+logging.basicConfig(level=logging.DEBUG, filename='app.log', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 
 if __name__ == "__main__":
-    main()
+    app.run(
+        debug = True,
+        host = "0.0.0.0",
+        port = 5000,
+        )
     GPIO.cleanup()
